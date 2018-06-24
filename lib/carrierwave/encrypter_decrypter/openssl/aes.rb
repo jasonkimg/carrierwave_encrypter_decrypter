159c70f392783638fab99094e6829f0e412fff32
j <j@dev5.net>
2018-06-24 03:17:07 +0000

    fix can't find filepath error when decrypt file

--- lib/carrierwave/encrypter_decrypter/openssl/aes.rb
+++ lib/carrierwave/encrypter_decrypter/openssl/aes.rb
@@ -39,9 +39,9 @@ module Openssl
         cipher.iv = model.iv
         cipher.key = model.key
         buf = ""
-
-        original_file_path =  obj.send(mounted_as).root + obj.send(mounted_as).url
-        encrypted_file_path =  obj.send(mounted_as).root + obj.send(mounted_as).url  + ".enc"
+        
+        original_file_path = obj.send(mounted_as).root.to_s + obj.send(mounted_as).url.to_s
+        encrypted_file_path = obj.send(mounted_as).root.to_s + obj.send(mounted_as).url.to_s + ".enc"
 
         File.open(original_file_path, "wb") do |outf|
           File.open(encrypted_file_path, "rb") do |inf|
